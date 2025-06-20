1a0e29562ba3d96065f8318b86e53994d963baa6
joao <jp.alvesrodrigues02@gmail.com>
2025-06-20 13:57:23 -0300

    chore(prisma): Install and configure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 import { UsersModule } from './users/infrastructure/users.module';
+import { DatabaseModule } from './shared/infrastructure/database/database.module';
+import { PrismaService } from './shared/infrastructure/database/prisma/prisma.service';
 
 @Module({
-  imports: [EnvConfigModule, UsersModule],
+  imports: [EnvConfigModule, UsersModule, DatabaseModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
