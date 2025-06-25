99d62a9dd2ca2ace1d804ee0998d3f375a91f718
joao <jp.alvesrodrigues02@gmail.com>
2025-06-25 10:04:37 -0300

    fix: Insert WrapperDataInterceptor in the right place to work correctly

--- src/global-config.ts
+++ src/global-config.ts
@@ -3,6 +3,8 @@ import { Reflector } from '@nestjs/core';
 import { WrapperDataInterceptor } from './shared/infrastructure/interceptors/wrapper-data/wrapper-data.interceptor';
 
 export function applyGlobalConfig(app: INestApplication) {
-  new WrapperDataInterceptor();
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalInterceptors(
+    new WrapperDataInterceptor(),
+    new ClassSerializerInterceptor(app.get(Reflector)),
+  );
 }
