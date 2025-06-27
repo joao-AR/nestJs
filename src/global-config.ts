825cd7fa0dfc52d4c36b3893a623fb31670d1538
joao <jp.alvesrodrigues02@gmail.com>
2025-06-27 12:42:31 -0300

    feat: Create NotFoundErrorFilter

--- src/global-config.ts
+++ src/global-config.ts
@@ -5,7 +5,8 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { WrapperDataInterceptor } from './shared/infrastructure/interceptors/wrapper-data/wrapper-data.interceptor';
-import { ConflictErrorFilter } from './shared/infrastructure/exception-filters/conflict-error/conflict-error.filter';
+import { ConflictErrorFilter } from './shared/infrastructure/exception-filters/conflict-error.filter';
+import { NotFoundErrorFilter } from './shared/infrastructure/exception-filters/not-found-error.filter';
 
 export function applyGlobalConfig(app: INestApplication) {
   app.useGlobalPipes(
@@ -16,13 +17,11 @@ export function applyGlobalConfig(app: INestApplication) {
       transform: true,
     }),
   );
-  
+
   app.useGlobalInterceptors(
     new WrapperDataInterceptor(),
     new ClassSerializerInterceptor(app.get(Reflector)),
   );
 
-  app.useGlobalFilters(
-    new ConflictErrorFilter()
-  )
+  app.useGlobalFilters(new ConflictErrorFilter(), new NotFoundErrorFilter());
 }
