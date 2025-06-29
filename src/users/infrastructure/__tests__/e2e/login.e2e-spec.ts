c13c710a5c551c0610e9771ff4f8b38feb932a43
joao <jp.alvesrodrigues02@gmail.com>
2025-06-29 12:27:09 -0300

    test: login e2e tests

---
+++ src/users/infrastructure/__tests__/e2e/login.e2e-spec.ts
@@ -0,0 +1,152 @@
+import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/testing/setup-prisma-tests';
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { EnvConfigModule } from '@/shared/infrastructure/env-config/env-config.module';
+import { UsersModule } from '../../users.module';
+import { DatabaseModule } from '@/shared/infrastructure/database/database.module';
+import request from 'supertest';
+import { UsersController } from '../../users.controller';
+import { instanceToPlain } from 'class-transformer';
+import { applyGlobalConfig } from '@/global-config';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { userDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+import { SigninDto } from '../../dto/signin-user.dto';
+import { BcryptjsHashProvider } from '../../providers/hash-provider/bcryptjs-hash.provider';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
+import { password } from '@inquirer/prompts';
+
+describe('UserController POST login e2e tests', () => {
+  let app: INestApplication;
+  let module: TestingModule;
+  let repository: UserRepository.Repository;
+  let hashProvider: HashProvider;
+  let signinDto: SigninDto;
+  const prismaService = new PrismaClient();
+
+  beforeAll(async () => {
+    setupPrismaTests();
+    module = await Test.createTestingModule({
+      imports: [
+        EnvConfigModule,
+        UsersModule,
+        DatabaseModule.forTest(prismaService),
+      ],
+    }).compile();
+
+    app = module.createNestApplication();
+    applyGlobalConfig(app);
+    await app.init();
+
+    repository = module.get<UserRepository.Repository>('UserRepository');
+    hashProvider = new BcryptjsHashProvider();
+  });
+
+  beforeEach(async () => {
+    signinDto = {
+      email: 't@t.com',
+      password: 'Pass123',
+    };
+
+    await prismaService.user.deleteMany();
+  });
+
+  describe('POST /users/login', () => {
+    it('Should authenticate a user', async () => {
+      const passHash = await hashProvider.generateHash(signinDto.password);
+      const entity = new UserEntity(
+        userDataBuilder({ email: signinDto.email, password: passHash }),
+      );
+      repository.insert(entity);
+      const res = await request(app.getHttpServer())
+        .post('/users/login')
+        .send(signinDto)
+        .expect(200);
+
+      expect(Object.keys(res.body)).toStrictEqual(['accessToken']);
+
+      expect(typeof res.body.accessToken).toEqual('string');
+    });
+
+    it('Should return error with 422 code when request body is invalid', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/users/login')
+        .send({})
+        .expect(422);
+
+      expect(res.body.error).toBe('Unprocessable Entity');
+      expect(res.body.message).toEqual([
+        'email must be an email',
+        'email should not be empty',
+        'email must be a string',
+        'password should not be empty',
+        'password must be a string',
+      ]);
+    });
+
+    it('Should return error with 422 code when email is invalid', async () => {
+      delete signinDto.email;
+      const res = await request(app.getHttpServer())
+        .post('/users/login')
+        .send(signinDto)
+        .expect(422);
+
+      expect(res.body.error).toBe('Unprocessable Entity');
+      expect(res.body.message).toEqual([
+        'email must be an email',
+        'email should not be empty',
+        'email must be a string',
+      ]);
+    });
+
+    it('Should return error with 422 code when password is invalid', async () => {
+      delete signinDto.password;
+      const res = await request(app.getHttpServer())
+        .post('/users/login')
+        .send(signinDto)
+        .expect(422);
+
+      expect(res.body.error).toBe('Unprocessable Entity');
+      expect(res.body.message).toEqual([
+        'password should not be empty',
+        'password must be a string',
+      ]);
+    });
+
+    it('Should return error with 404 code when email is not found', async () => {
+      delete signinDto.email;
+      const res = await request(app.getHttpServer())
+        .post('/users/login')
+        .send({
+          email: 'fakeEmail@t.com',
+          password: '1234',
+        })
+        .expect(404);
+      console.log(res.body);
+      expect(res.body.error).toBe('Not Found');
+      expect(res.body.message).toEqual(
+        'UserModel not found using email fakeEmail@t.com',
+      );
+    });
+
+    it('Should return error with 400 code when password is incorrect', async () => {
+      const passHash = await hashProvider.generateHash(signinDto.password);
+      const entity = new UserEntity(
+        userDataBuilder({ email: signinDto.email, password: passHash }),
+      );
+      repository.insert(entity);
+
+      const res = await request(app.getHttpServer())
+        .post('/users/login')
+        .send({
+          email: signinDto.email,
+          password: 'fakePass',
+        })
+        .expect(400);
+
+      expect(res.body.error).toBe('Bad Request');
+      expect(res.body.message).toEqual('Invalid credentials');
+    });
+  });
+});
