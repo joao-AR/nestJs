ef2a7e100c2750821ad06be927921b921d4cd859
João Pedro Rodrigues <jp.alvesrodrigues02@gmail.com>
2026-03-28 10:56:48 -0300

    refactor (database): Remove import not used

--- src/shared/infrastructure/database/database.module.ts
+++ src/shared/infrastructure/database/database.module.ts
@@ -1,11 +1,9 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
-import { EnvConfigModule } from '../env-config/env-config.module';
 import { PrismaClient } from '@prisma/client';
 
 @Global()
 @Module({
-  imports: [EnvConfigModule.forRoot()],
   providers: [PrismaService],
   exports: [PrismaService],
 })
