73ae51d636e104268dd15ac43f8605400b7bf9ba
João Pedro Rodrigues <jp.alvesrodrigues02@gmail.com>
2026-03-29 14:34:56 -0300

    test(prisma): update setup script to inherit process.env and stdio

--- src/shared/infrastructure/database/prisma/testing/setup-prisma-tests.ts
+++ src/shared/infrastructure/database/prisma/testing/setup-prisma-tests.ts
@@ -2,6 +2,10 @@ import { execSync } from 'node:child_process';
 
 export function setupPrismaTests() {
   execSync(
-    'npx dotenv-cli -e .env.test -- npx prisma migrate dev --schema ./src/shared/infrastructure/database/prisma/schema.prisma',
+    'npx prisma migrate dev --schema ./src/shared/infrastructure/database/prisma/schema.prisma',
+    {
+      stdio: 'inherit',
+      env: process.env,
+    },
   );
 }
