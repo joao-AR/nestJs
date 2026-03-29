bd40bdc72f491571f4e4125eaeebd05c615111e7
João Pedro Rodrigues <jp.alvesrodrigues02@gmail.com>
2026-03-29 14:36:29 -0300

    refactor(database): remove forTestMethod thats no longer needed

--- src/shared/infrastructure/database/database.module.ts
+++ src/shared/infrastructure/database/database.module.ts
@@ -1,22 +1,9 @@
-import { DynamicModule, Global, Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
-import { PrismaClient } from '@prisma/client';
 
 @Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
 })
-export class DatabaseModule {
-  static forTest(prismaClient: PrismaClient): DynamicModule {
-    return {
-      module: DatabaseModule,
-      providers: [
-        {
-          provide: PrismaService,
-          useFactory: () => prismaClient as PrismaService,
-        },
-      ],
-    };
-  }
-}
+export class DatabaseModule {}
