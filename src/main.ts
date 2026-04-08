982b25ca9cf9d8916f5d0a3be71695c979d9100a
João Pedro Rodrigues <jp.alvesrodrigues02@gmail.com>
2026-04-08 10:38:14 -0300

    feat: Install swagger

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,33 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { applyGlobalConfig } from './global-config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest default template')
+    .setDescription(
+      'Nest template Rest API, Typescript, DDD, Clean architecture',
+    )
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      description: 'Supply JWT to authorize',
+      name: 'Authorization',
+      bearerFormat: 'JWT',
+      scheme: 'bearer',
+      type: 'http',
+      in: 'header',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   applyGlobalConfig(app);
   await app.listen(3000, '0.0.0.0');
 }
