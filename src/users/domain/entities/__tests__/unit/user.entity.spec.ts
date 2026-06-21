bf4ef7d857f09c03ba322170a3cfdd40735308fd
João Pedro Rodrigues <jp.alvesrodrigues02@gmail.com>
2026-06-21 18:53:59 +0000

    test(users): fix unbound method eslint error using jest.spyOn

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,10 +1,15 @@
 import { UserEntity, UserProps } from '../../user.entity';
 import { userDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+
 describe('User entity unit tests', () => {
   let props: UserProps;
   let sut: UserEntity;
+  let validateSpy: jest.SpyInstance;
+
   beforeEach(() => {
-    UserEntity.validate = jest.fn();
+    validateSpy = jest
+      .spyOn(UserEntity, 'validate')
+      .mockImplementation(() => {});
     props = userDataBuilder({});
     sut = new UserEntity(props);
   });
@@ -13,7 +18,7 @@ describe('User entity unit tests', () => {
     expect(sut.name).toEqual(props.name);
     expect(sut.email).toEqual(props.email);
     expect(sut.createdAt).toBeInstanceOf(Date);
-    expect(UserEntity.validate).toHaveBeenCalled();
+    expect(validateSpy).toHaveBeenCalled();
   });
 
   it('Getter of name field', () => {
@@ -53,13 +58,13 @@ describe('User entity unit tests', () => {
 
   it('Should update a user', () => {
     sut.updateName('new name');
-    expect(UserEntity.validate).toHaveBeenCalled();
+    expect(validateSpy).toHaveBeenCalled();
     expect(sut.name).toEqual('new name');
   });
 
   it('Should update the password field', () => {
     sut.updatePassword('new pass');
-    expect(UserEntity.validate).toHaveBeenCalled();
+    expect(validateSpy).toHaveBeenCalled();
     expect(sut.password).toEqual('new pass');
   });
 });
